e242208121dc9d49271e7ecfad5f345e66feb652
nishantparihar <nishprihar@gmail.com>
2024-05-17 16:01:57 +0530

    Intialized base components in app.jsx

--- fe/src/App.jsx
+++ fe/src/App.jsx
@@ -1,14 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Header  from './Header'
+import Body from './Body'
+import Footer from './Footer'
+
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      
+      <Header />
+      <Body />
+      <Footer />
     </>
   )
 }
